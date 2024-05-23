b2d491f97f2431987e53794150be07154bcbb523
Александр Курчаков <fistin_103@mail.ru>
2024-05-23 18:17:18 +0500

    save

--- project/src/utils/tokenUtils.js
+++
@@ -1,7 +0,0 @@
-function generateToken() {
-  return Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
-}
-
-module.exports = {
-  generateToken
-};
--- src/controllers/startController.js
+++ src/controllers/startController.js
@@ -1,217 +1,187 @@
+const express = require("express");
 const axios = require("axios");
-const { botToken, groupChatId, oneTimeLinks } = require("../config/config");
-const path = require("path");
-const FormData = require("form-data");
-const { createChatAndInviteLink } = require("../utils/chatUtils");
-const { generateToken } = require("../utils/tokenUtils");
-const fs = require("fs");
-const { User } = require("../models/users.model");
-const { Form } = require("../models/GoggleForm.model");
-const { exportAnswers } = require("../controllers/quiz");
+const { checkConnection } = require("./controllers/botactive");
+const { botToken, groupChatId, userId } = require("./config/config");
+const { User } = require("./models/users.model");
+const mongoose = require("mongoose");
+const { handleStartCommand } = require("./controllers/startController");
+const { handleCommand } = require("./controllers/commandController");
+const { exportAnswers } = require("./controllers/quiz");
 
-async function updateFormPeriodically() {
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+// Обработчик для всех команд
+app.post("/commands", async (req, res) => {
   try {
-    const answers = await exportAnswers(); // Получаем ответы из Google Forms
-    console.log("Answers:", answers);
-
-    await Promise.all(
-      answers.map(async (answer) => {
-        const existingForm = await Form.findOne({ nameForm: answer.name });
-
-        if (existingForm) {
-          // Если найдена существующая запись с именем, обновляем данные
-          await Form.findOneAndUpdate(
-            { nameForm: answer.name },
-            {
-              $addToSet: { genderForm: answer.gender },
-              $addToSet: { ageForm: answer.age },
-            },
-            { new: true }
-          );
-          console.log(`Updated Form for '${answer.name}'`);
-        } else {
-          // Если запись не найдена, создаем новую
-          const newForm = new Form({
-            nameForm: answer.name,
-            genderForm: [answer.gender],
-            ageForm: [answer.age],
-          });
-          await newForm.save();
-          console.log(`New Form data saved for '${answer.name}'`);
-        }
-      })
-    );
-  } catch (error) {
-    console.error("Error updating Form:", error);
-  }
-}
-
-// Устанавливаем интервал выполнения функции updateFormPeriodically каждые 250 секунд (250000 миллисекунд)
-setInterval(updateFormPeriodically, 250000);
-
-async function handleStartCommand(req, res) {
-  try {
-    const { message } = req.body;
-
-    if (!message || !message.text) {
+    const channel_post = req.body;
+    if (!channel_post || !channel_post.message) {
+      console.error("Invalid channel_post data or missing message");
       return res
         .status(400)
-        .json({ success: false, message: "Invalid message format." });
+        .json({ success: false, message: "Invalid request data." });
     }
 
-    const userId = message.from.id;
-    const firstName = message.from.first_name;
+    const chatId = channel_post.message.chat
+      ? channel_post.message.chat.id
+      : null;
+    const unknownCommandText = `Команда не распознана. Пожалуйста, попробуйте другую команду.`;
+    console.log("Я ТУТ ВЫВОЖУ channel_post:", channel_post);
+    console.log("я тело:", req.body);
 
-    const answers = await exportAnswers();
-    console.log(answers);
-
-    let userDataToUpdate = {
-      firstName: message.from.first_name || "",
-      lastName: message.from.last_name || "",
-      userName: "@" + message.from.username || "",
-      location: message.from.location || {},
-      phoneNumber: message.from.phone_number || "",
-      birthDate: "",
-      friends: message.from.friends || [],
-      address: message.from.address || "",
-      email: message.from.email || "",
-      contact: message.from.contact || "",
-      contacts: message.from.contacts || [],
-      date: message.date || 0,
-      isBot: message.from.is_bot || false,
-      gender: message.from.gender,
-      nameForm: "", // Используем данные из answers
-      genderForm: [], // Используем массив для данных по полу из answers
-      ageForm: [], // Используем массив для данных по возрасту из answers
-    };
-
-    const usernameWithoutAt = userDataToUpdate.userName.slice(1); // Имя пользователя без символа "@"
-
-    const matchingAnswers = answers.filter((answer) => {
-      return answer.name === usernameWithoutAt; // Находим соответствующий ответ из Google Forms
-    });
-
-    console.log("Matching answers:", matchingAnswers);
-
-    if (matchingAnswers.length === 0) {
-      // Если не найдены совпадающие ответы из Google Forms
-      console.log("No matching answers found for", usernameWithoutAt);
-    }
-
-    matchingAnswers.forEach((answer) => {
-      userDataToUpdate.nameForm = answer.name;
-      userDataToUpdate.genderForm.push(answer.gender); // Добавляем значение gender в массив genderForm
-      userDataToUpdate.ageForm.push(answer.age); // Добавляем значение age в массив ageForm
-    });
-
-    console.log("Updated userDataToUpdate:", userDataToUpdate);
-
-    // Очистка объекта userDataToUpdate от пустых строк и некорректных значений
-    userDataToUpdate = cleanObject(userDataToUpdate);
-
-    let existingUser = await User.findOne({ userId });
-
-    if (existingUser) {
-      // Обновляем существующего пользователя
-      existingUser.set(userDataToUpdate); // Применяем только обновленные поля
-      await existingUser.save();
-    } else {
-      // Создаем нового пользователя
-      const newUser = new User({
-        userId,
-        ...userDataToUpdate,
-      });
-      await newUser.save();
-    }
-
-    const command = message.text.trim().toLowerCase();
-
-    if (command === "/start") {
-      // Отправка сообщения с пригласительной ссылкой и меню
-      const inviteLink = await createChatAndInviteLink(botToken, groupChatId);
-      const token = generateToken();
-      oneTimeLinks[token] = inviteLink; // Сохраняем одноразовую ссылку
-
-      const menuText = `Команды:\n/tarif - Опция 1\n/support - Опция 2\n/about - Опция 3\n/tt - Опция 4\n/order - Опция 5`;
-      const imagePath = path.join(__dirname, "welcome.jpg");
-
-      const caption =
-        `Добро пожаловать, *${firstName}*!🔥\n\n` +
-        `Рады приветствовать вас в мире инноваций и творчества от *QazSoft!* Я ваш личный помощник, *Baimo*, и я здесь, чтобы сделать ваше взаимодействие с нашей веб-студией максимально приятным и продуктивным.\n\n` +
-        `Вот ваша пригласительная ссылка: ${inviteLink}\n\n` +
-        `Меня зовут *Baimo* к вашим услугам.\n\n` +
-        `Что я могу для вас сделать:\n\n` +
-        `📞 *Техническая поддержка:* Быстрое решение любых технических вопросов.\n` +
-        `💻 *Информация о наших продуктах:* Узнайте больше о том, что мы предлагаем.\n` +
-        `🛒 *Оформление заказов:* Помощь в заказе наших услуг и продуктов.\n\n` +
-        `Опросник для улучшения услуг */quiz.*\n` +
-        `Введите команду */help*, чтобы узнать все мои команды.\n\n` +
-        `Спасибо, что выбрали QazSoft. Я уверен, что наше сотрудничество будет плодотворным и успешным. Давайте начнем это увлекательное путешествие вместе!\n\n` +
-        `*Присоединяйтесь к нашему сообществу и будьте в курсе всех новостей и обновлений:*`;
-
-      const formData = new FormData();
-      formData.append("chat_id", userId);
-      formData.append("photo", fs.createReadStream(imagePath));
-      formData.append("caption", caption);
-      formData.append("parse_mode", "Markdown");
-
-      const replyMarkup = {
-        inline_keyboard: [
-          [
-            { text: "Канал", url: inviteLink },
-            { text: "Сайт", url: "https://www.instagram.com/qazsoft.kz/" },
-          ],
-          [{ text: "Инстаграм", url: "https://www.instagram.com/qazsoft.kz/" }],
-        ],
-      };
-
-      await axios.post(
-        `https://api.telegram.org/bot${botToken}/sendPhoto`,
-        formData,
-        {
-          headers: formData.getHeaders(),
-          params: { reply_markup: JSON.stringify(replyMarkup) },
-        }
-      );
-
-      res.status(200).json({
-        success: true,
-        message: "Invite link and menu sent successfully.",
-      });
-    } else {
-      const unknownCommandText = `Команда не распознана. Пожалуйста, попробуйте другую команду.`;
+    if (
+      channel_post.message.voice ||
+      channel_post.message.document ||
+      channel_post.message.photo ||
+      channel_post.message.video ||
+      channel_post.message.poll ||
+      channel_post.message.sticker ||
+      channel_post.message.location ||
+      channel_post.message.gift ||
+      channel_post.message.file ||
+      channel_post.message.audio ||
+      !channel_post
+    ) {
+      // Обработка голосового сообщения
       await axios.post(`https://api.telegram.org/bot${botToken}/sendMessage`, {
-        chat_id: userId,
+        chat_id: chatId,
         text: unknownCommandText,
       });
+      console.log("Received voice message. Ignoring.");
+      return res
+        .status(200)
+        .json({ success: true, message: "Voice message ignored." });
+    }
 
-      res.status(200).json({
-        success: true,
-        message: "Unknown command processed successfully.",
-      });
+    // Обработка текстового сообщения
+    const command = channel_post.message.text.trim();
+
+    console.log("СМОТРЕТЬ ТУТ", channel_post.message.text);
+
+    switch (command) {
+      case "/start":
+        await handleStartCommand(req, res);
+        break;
+      case "/order":
+      case "/tarif":
+      case "/tt":
+      case "/help":
+      case "/about":
+      case "/help":
+      case "/support":
+      case "/quiz":
+        await handleCommand(req, res);
+        break;
+      // case "/quiz":
+      //   await handleQuizCommand(req, res);
+      //   break;
+      default:
+        if (
+          channel_post.message &&
+          channel_post.message.chat &&
+          channel_post.message.chat.id
+        ) {
+          await sendMessageError(chatId, unknownCommandText, res);
+        } else {
+          console.error("Invalid channel_post data or missing chat id");
+          res
+            .status(500)
+            .json({ success: false, message: "Invalid request data." });
+        }
+        break;
     }
   } catch (error) {
-    console.error("Error handling /start command:", error);
+    console.error("Error handling command:", error);
     res.status(500).json({ success: false, message: "An error occurred." });
   }
-}
+});
 
-// Функция для удаления пустых строк и некорректных значений из объекта
-function cleanObject(obj) {
-  const cleaned = {};
-  for (const [key, value] of Object.entries(obj)) {
-    if (
-      value !== undefined &&
-      value !== null &&
-      value !== "" &&
-      !(Array.isArray(value) && value.length === 0)
-    ) {
-      cleaned[key] = value;
+// Функция отправки сообщения об ошибке
+async function sendMessageError(chatId, text, res) {
+  try {
+    const response = await axios.post(
+      `https://api.telegram.org/bot${botToken}/sendMessage`,
+      {
+        chat_id: chatId,
+        text: text,
+      }
+    );
+
+    if (response.data.ok) {
+      res.status(200).json({
+        success: true,
+        message: "Message sent successfully.",
+      });
+    } else {
+      console.error("Failed to send message:", response.data.description);
+      res
+        .status(500)
+        .json({ success: false, message: "Failed to send message." });
     }
+  } catch (error) {
+    console.error("Error sending message:", error);
+    res.status(500).json({
+      success: false,
+      message: "An error occurred while sending message.",
+    });
   }
-  return cleaned;
 }
 
-module.exports = {
-  handleStartCommand,
+// Установка команд для бота
+const setCommands = async () => {
+  try {
+    const commands = [
+      { command: "/start", description: "Начать" },
+      { command: "/order", description: "Сделать заказ" },
+      { command: "/tarif", description: "Узнать тарифы" },
+      { command: "/tt", description: "Что нужно для Технического Задания?" },
+      { command: "/quiz", description: "Пройти опрос из 3 вопросов" },
+      { command: "/about", description: "Информация о QazSoft" },
+      { command: "/help", description: "Все команды" },
+      { command: "/support", description: "Поддержать QazSoft" },
+    ];
+
+    const response = await axios.post(
+      `https://api.telegram.org/bot${botToken}/setMyCommands`,
+      { commands: commands }
+    );
+
+    if (response.data.ok) {
+      console.log("Commands set successfully.");
+    } else {
+      console.error("Failed to set commands:", response.data.description);
+    }
+  } catch (error) {
+    console.error("Error setting commands:", error);
+  }
 };
+
+// Вызов функции для установки команд
+setCommands();
+
+// Обработчик для добавления пользователя в базу данных
+app.post("/database/user", async (req, res) => {
+  try {
+    const user = await User.create(req.body);
+    res.status(200).json({ message: "User added to collection", user });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+// Запуск сервера Express
+app.listen(port, async () => {
+  console.log(`Server is running on port ${port}`);
+
+  mongoose.connect(
+    "mongodb+srv://admin:1q4r6y1q4r6y@backdb.bgdmu1s.mongodb.net/database?retryWrites=true&w=majority&appName=BackDb"
+  );
+
+  // Периодический запуск экспорта ответов
+  setInterval(exportAnswers, 300000);
+
+  // Первоначальная проверка состояния при запуске сервера
+  await checkConnection();
+});
